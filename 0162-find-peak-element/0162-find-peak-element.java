7a22e154fc2ff8e81c1fd977e4bffab769cdb025
Eijkeyal Pakhrin <209868212+Eijkeyal@users.noreply.github.com>
2025-12-09 23:32:20 +0545

    Time: 0 ms (100%), Space: 44.3 MB (15.03%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -6,10 +6,10 @@ class Solution {
         while(start<end){
             int mid =start + (end - start)/2;
             if(nums[mid]<=nums[mid+1]){
-                start = mid +1;
+                start = mid +1;//search if peak is to the right
                // result =mid; 
             } else{
-                end = mid;
+                end = mid;// peak could be the left
             }
         }
         return start;
