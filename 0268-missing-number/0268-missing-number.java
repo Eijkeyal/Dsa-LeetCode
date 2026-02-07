a1ca8998c23acafbddb2fb86084a56c15a625ee9
Eijkeyal Pakhrin <209868212+Eijkeyal@users.noreply.github.com>
2026-02-07 22:16:46 +0545

    Time: 9 ms (8.12%), Space: 48.2 MB (8.12%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,6 +1,6 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        // For ascending order
+        /*For ascending order
         Arrays.sort(nums);
     // applying Binary Search algorithim
         int start = 0;
@@ -15,5 +15,18 @@ class Solution {
             }
          }
          return start;
+         */
+         Arrays.sort(nums);//sort the array because Binary search wont work on unsorted array
+         int start = 0;
+         int end = nums.length-1;
+         while(start<=end){
+            int mid = start + (end - start)/2;
+            if(nums[mid]==mid){
+                start = mid +1;//checks the mid in nums[mid]. if it is true then increse by one and checks for another nums[mid] to mid
+            } else{
+                end = mid -1;
+            }
+         }
+         return start;// return the value of start because it sotres the current the index value which is compared to the mid which is supposed to be missing numbers
     }
 }
