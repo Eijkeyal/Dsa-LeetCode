f149f61a225c3cf763779ba863cbf056d3b8b2d7
Eijkeyal Pakhrin <209868212+Eijkeyal@users.noreply.github.com>
2026-02-10 14:20:30 +0545

    Time: 0 ms (100%), Space: 42.5 MB (15.98%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,5 +1,5 @@
 class Solution {
-    public boolean isPerfectSquare(int num) {
+    public boolean isPerfectSquare(int num) {/*
       if(num<0) return false;
       if(num==0 || num ==1) return true;
       int start = 1;
@@ -15,6 +15,23 @@ class Solution {
             end = mid -1;
         }
       }
-      return false;
+      return false;*/
+      //This is the questions no 367 valid Perfect Square by using Binary Search Algorithm
+      if(num<0) return false;// this is because we only need positive integer(non negattive integer) not a negative integer
+      if(num==0 || num==1) return true;//this checks is this nums perfect square or not 
+      int start = 1;
+      int end = num/2;
+      while(start<=end){
+        int mid = start + (end - start)/2;
+        long square = (long)mid*mid;// this is usually done to prevent overflow in long size of integer
+        if(square == num){
+            return true;
+        }else if(square<num){
+            start = mid+1;
+        }else {
+            end = mid -1;
+        }
+      }
+      return false; // if any of condition failed it return false 
     }
 }
