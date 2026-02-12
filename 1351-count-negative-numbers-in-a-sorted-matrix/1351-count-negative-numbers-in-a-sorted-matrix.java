0b36b59c43f30c505abea1920b98f2688fea47dc
Eijkeyal Pakhrin <209868212+Eijkeyal@users.noreply.github.com>
2026-02-12 23:51:35 +0545

    Update 1351-count-negative-numbers-in-a-sorted-matrix.java

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.java
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.java
@@ -1,5 +1,6 @@
 class Solution{
     public int countNegatives(int[][] grid){
+        //this is the problem that is solved by using two pointers approach
         int count = 0;
         int rows = grid.length;
         int cols = grid[0].length;
@@ -18,6 +19,16 @@ class Solution{
             }else{
                 row--;
                 col = cols-1;
+            }
+        }
+        return count;
+        //if i had to solve this problem by another by sing nested loop
+        int count = 0;
+        for(int i = 0;i<grid.length;i++){
+            for(int j = 0;j<grid[i].length;j++){
+                if(grid[i][j]>0){//it only count when the condition grid[i][j] is grester tham 0 otherwise it si negative numbers
+                    count++;
+                }
             }
         }
         return count;
