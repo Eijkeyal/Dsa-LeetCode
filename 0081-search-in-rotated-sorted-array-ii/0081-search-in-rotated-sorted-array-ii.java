433e507421f828eb2dcf400f9c62b207aa35358a
Eijkeyal Pakhrin <209868212+Eijkeyal@users.noreply.github.com>
2026-02-13 22:30:52 +0545

    Update 0081-search-in-rotated-sorted-array-ii.java

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean search(int[] nums, int target) {
+        //this problem is solved by usnig two algorithim which is Two Pointers and Binary Search 
         int start = 0;
         int end = nums.length-1;
         while(start<=end){
@@ -28,5 +29,26 @@ class Solution {
             }
         }
         return false;
+    }
+
+//Here i want to solve same problem by new approach 
+class Solution{
+    public boolean search(int[] nums, int target){
+        //using ony binary search algorithim
+        Arrays.sort(nums);//sort the array in ascending order
+        int start = 0;
+        int end = nums.length-1;
+        while(start<=end){
+            int mid = start + (end - start)/2;
+            if(nums[mid]==target){// if nums[mid] is equals to the Target then return the True other wise search either left or right
+                return true;
+            }else if(nums[mid]>target){
+                end = mid -1;
+            }else{
+                start = mid + 1;
+            }
+        }
+        return false;// if there is none of conditions meets the targeted element the return fale it shows there is no element that meet the conditiosn
+        
     }
 }
