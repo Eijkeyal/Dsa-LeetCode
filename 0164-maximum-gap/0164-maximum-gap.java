1433180944dae47b3a9f8ebbb2903c953a5d13f6
Eijkeyal Pakhrin <209868212+Eijkeyal@users.noreply.github.com>
2026-02-28 18:15:55 +0545

    Time: 44 ms (75.85%), Space: 90.8 MB (75.89%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.java
+++ 0164-maximum-gap/0164-maximum-gap.java
@@ -1,15 +1,19 @@
 class Solution {
     public int maximumGap(int[] nums) {
+        //Step 1: Sort the array
         Arrays.sort(nums);
+        //Step 2: Initialize both gap and maxGap
         int gap = 0;
         int maxGap = 0;
         for(int i = 0;i<nums.length -1; i++){
+            //Step 3: Calculate the gap between nearest nums[i] and nums[i + 1]
             gap = nums[i + 1] - nums[i];
+            //Step 4 :If new gap is greater than previous gap(maxGap) then assign maxGap as gap
             if(gap>maxGap){
                 maxGap = gap;
-                //return maxGap;
             }
         }
+        //Step 4: Update the maximum gap of element
         return maxGap;
     }
 }
