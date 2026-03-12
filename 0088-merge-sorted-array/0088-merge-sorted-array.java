2080265b2cf47f7a1b16ce926416d8b45a98d9e7
Eijkeyal Pakhrin <209868212+Eijkeyal@users.noreply.github.com>
2026-03-12 22:32:42 +0545

    Time: 0 ms (100%), Space: 44 MB (40.61%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         //start from the end
@@ -10,7 +11,7 @@ class Solution {
                 nums1[k]=nums1[i];
                 i--;
             } else {
-                nums1[k] = nums2[j];
+                nums1[k] = nums2[j];  
                 j--;
             }
             k--;
@@ -20,6 +21,31 @@ class Solution {
             nums1[k] = nums2[j];
             j--;
             k--;// no need to copy remaining nums1 elements they are already in place
+        }
+    }
+}
+*/
+class Solution{
+    public void merge(int[] nums1, int m, int[] nums2, int n){
+        int result[] = new int[m+n];
+        int i = 0; 
+        int j = 0;
+        int k = 0;
+        while(i<m && j<n){
+            if(nums1[i]<nums2[j]){
+                result[k++] = nums1[i++];
+            }else{
+                result[k++] = nums2[j++];
+            }
+        }
+        while(i<m){
+            result[k++] = nums1[i++];
+        }
+        while(j<n){
+            result[k++] = nums2[j++]; 
+        }
+        for(int x = 0; x<m+n;x++){
+            nums1[x] = result[x];
         }
     }
 }
