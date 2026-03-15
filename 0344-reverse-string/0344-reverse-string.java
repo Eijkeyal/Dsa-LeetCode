82124e12c66f11068319fd53b960c20505c0d9cd
Eijkeyal Pakhrin <209868212+Eijkeyal@users.noreply.github.com>
2026-03-15 23:12:10 +0545

    Time: 0 ms (100%), Space: 48.1 MB (93.19%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,4 +1,4 @@
-class Solution {
+/*(class Solution {
     public void reverseString(char[] s) {
         char reversed[] = new char[s.length];
         for(int i=0;i<s.length;i++){
@@ -6,6 +6,23 @@ class Solution {
         }
         for(int i=0;i<s.length;i++){// this iterations replaces the reversed with original one
             s[i] = reversed[i];
+        }
+    }
+}*/
+class Solution{
+    public void reverseString(char[] s){
+        //Step1 : Initialize two pointer
+        int i = 0;
+        int j = s.length-1;
+        //Step2 : Check conditions
+        while(i<j){
+            //Steps3: Swap the character
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            //Increase the i pointer by 1 and descrease the j pointer by 1
+            i++;
+            j--;
         }
     }
 }
