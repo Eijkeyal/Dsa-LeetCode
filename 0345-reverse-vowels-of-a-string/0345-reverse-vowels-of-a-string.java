e840721b92d5f706096fddb16c40ca7150266cc4
Eijkeyal Pakhrin <209868212+Eijkeyal@users.noreply.github.com>
2026-03-16 06:49:02 +0545

    Time: 3 ms (76.55%), Space: 46.5 MB (78.39%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public String reverseVowels(String s) {
         char[] chars = s.toCharArray();
         int left = 0;
@@ -33,5 +33,36 @@ class Solution {
         c = Character.toLowerCase(c);
         // Check if it's a vowel
         return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+}*/
+class Solution {
+    public String reverseVowels(String s) {
+        char[] chars = s.toCharArray();
+        String vowels = "aeiouAEIOU";
+        
+        int left = 0;
+        int right = s.length() - 1;
+        
+        while(left < right) {
+            // Find vowel from left
+            while(left < right && vowels.indexOf(chars[left]) == -1) {
+                left++;
+            }
+            
+            // Find vowel from right
+            while(left < right && vowels.indexOf(chars[right]) == -1) {
+                right--;
+            }
+            
+            // Swap vowels
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+            
+            left++;
+            right--;
+        }
+        
+        return new String(chars);
     }
 }
