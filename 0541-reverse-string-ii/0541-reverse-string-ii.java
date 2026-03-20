ecca1e8e516aa280eacf8fc8a6358524ca55477b
Eijkeyal Pakhrin <209868212+Eijkeyal@users.noreply.github.com>
2026-03-20 22:47:59 +0545

    Time: 1 ms (94.87%), Space: 44.8 MB (58.28%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.java
+++ 0541-reverse-string-ii/0541-reverse-string-ii.java
@@ -1,5 +1,6 @@
 class Solution {
     public String reverseStr(String s, int k) {
+        /*
         char[] arr = s.toCharArray();
         int n = arr.length;
         
@@ -13,6 +14,38 @@ class Solution {
                 left++;
                 right--;
             }
+        }
+        
+        return new String(arr);
+        */
+        char[] arr = s.toCharArray();
+        // Two pointers for entire string
+        int i = 0;  // Points to start of each chunk
+        int j = 0;  // Points to end of reverse section
+        
+        while(i < arr.length) {
+            // Set j to the end of the segment to reverse
+            j = i + k - 1;
+            
+            // If j goes beyond array, set to last index
+            if(j >= arr.length) {
+                j = arr.length - 1;
+            }
+            
+            // Reverse from i to j
+            int left = i;
+            int right = j;
+            
+            while(left < right) {
+                char temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+            
+            // Move to next chunk (2k steps forward)
+            i = i + 2*k;
         }
         
         return new String(arr);
