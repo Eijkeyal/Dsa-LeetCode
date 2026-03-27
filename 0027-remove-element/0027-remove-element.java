ab19900547723894dff8263e4fd1cc75602cc080
Eijkeyal Pakhrin <209868212+Eijkeyal@users.noreply.github.com>
2026-03-27 22:54:05 +0545

    Time: 0 ms (100%), Space: 43.5 MB (54.48%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -11,11 +11,15 @@ class Solution {
         }
         return k;
         */
+        //Initialize two pointer
         int i = 0;
         int k = 0;
         for(i = 0; i<nums.length; i++){
+            // If the current element is NOT the one we want to remove
             if(nums[i] != val){
+                // Move the valid element to the 'k' position
                 nums[k] = nums[i];
+                // Increment k to prepare for the next valid element
                 k++;
             }
         }
