ffa70fcd8e30b10686b1dd13bfde921bca9e8920
Eijkeyal Pakhrin <209868212+Eijkeyal@users.noreply.github.com>
2026-04-08 22:03:56 +0545

    Time: 107 ms (28.02%), Space: 19.7 MB (66.38%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        result = []
+        #loop through all people
+        for _ in range(len(heights)):
+            max_height = max(heights)#find tallest heights
+            index = heights.index(max_height)#get index of tallest
+
+            result.append(names[index])#add corresponding names
+            heights.pop(index)#remove used height
+            names.pop(index)#remove used name
+        return result
