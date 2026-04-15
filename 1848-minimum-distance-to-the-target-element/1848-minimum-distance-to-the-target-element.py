9e6bfdf06b90397152ee1505358b32972a973593
Eijkeyal Pakhrin <209868212+Eijkeyal@users.noreply.github.com>
2026-04-15 22:50:07 +0545

    Time: 1 ms (31.49%), Space: 12.4 MB (57.93%) - LeetHub

---
+++ 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def getMinDistance(self, nums, target, start):
+        """
+        :type nums: List[int]
+        :type target: int
+        :type start: int
+        :rtype: int
+        """
+        #using two pointer approach
+        left =start
+        right = start
+        while left>=0 or right<len(nums):
+            if left>=0 and nums[left] == target:
+                return start - left
+            if right<len(nums) and nums[right] == target:
+                return right-start
+            left = left -1
+            right = right + 1
+        return -1
