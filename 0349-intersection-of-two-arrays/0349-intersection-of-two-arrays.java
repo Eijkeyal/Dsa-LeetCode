e063b1e90f5769345573f867e6a5113c360e0cd1
Eijkeyal Pakhrin <209868212+Eijkeyal@users.noreply.github.com>
2026-05-02 23:33:23 +0545

    Time: 7 ms (17.79%), Space: 45.1 MB (41.22%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {/*
         // using hashSet to store results in hashset for uniqueness
         HashSet<Integer> resultSet = new HashSet<>();
@@ -17,7 +17,7 @@ class Solution {
             result[index++] = num;
         }
         return result;
-        */
+        *
         Arrays.sort(nums1);//Sort the nums1 Array
         Set<Integer> result = new HashSet<>();// use a set to store unique intersection elements 
         //Set data type doesn't allow the duplicates values 
@@ -45,4 +45,60 @@ class Solution {
        }
        return arr;//return the intersection of array
     }
+}
+*
+class Solution{
+    public int[] intersection(int[] nums1, int[] nums2){
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0; 
+        int j = 0;
+        Set<Integer> resultSet = new HashSet<>();
+        while(i<nums1.length && j<nums2.length){
+            if(nums1[i]==nums2[j]){
+                resultSet.add(nums1[i]);
+                i++;
+                j++;
+            }else if(nums1[i]<nums2[j]){
+                i++;
+            }else{
+                j++;
+            }
+        }
+        int result[] = new int[resultSet.size()];
+        int index = 0;
+        for(int num:resultSet){
+            result[index++] = num;
+        }
+        return result;
+    }
+}
+*/
+//usint two pointer approach
+class Solution{
+    public int[] intersection(int[] nums1, int [] nums2){
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0;
+        int j = 0;
+        List<Integer> result = new ArrayList<>();
+        while(i<nums1.length && j<nums2.length){
+            if(nums1[i] == nums2[j]){
+            if(result.isEmpty() || result.get(result.size() - 1) != nums1[i]) {
+                    result.add(nums1[i]);
+                }
+                i++;
+                j++;
+            }else if(nums1[i]<nums2[j]){
+                i++;
+            }else{
+               j++; 
+            }
+        }
+        int[] output = new int[result.size()];
+        for (int k = 0; k<result.size(); k++){
+            output[k] = result.get(k);
+        }
+        return output;
+    } 
 }
