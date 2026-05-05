962380e4b432c971ad8ca02a721920df56a52c50
Eijkeyal Pakhrin <209868212+Eijkeyal@users.noreply.github.com>
2026-05-05 23:47:14 +0545

    Time: 0 ms (100%), Space: 43.4 MB (93.9%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,6 +1,19 @@
 class Solution {
     public int findMin(int[] nums) {
+        /* This solutions works but i need to do on optimal solutions this is nlogn i need to do in log n
         Arrays.sort(nums);
         return nums[0];
+        */
+        int start = 0;
+        int end = nums.length -1;
+        while(start<end){
+            int mid = start + (end - start)/2;
+            if(nums[mid]>nums[end]){
+                start = mid + 1; 
+            }else{
+                end = mid;
+            }
+        }
+        return nums[start];
     }
 }
