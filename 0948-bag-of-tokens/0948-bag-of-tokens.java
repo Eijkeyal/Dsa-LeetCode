968c152a7b99e6de42a4885a895213825b1d0a2b
Eijkeyal Pakhrin <209868212+Eijkeyal@users.noreply.github.com>
2026-05-21 23:20:11 +0545

    Time: 6 ms (77.04%), Space: 45.1 MB (55.91%) - LeetHub

---
+++ 0948-bag-of-tokens/0948-bag-of-tokens.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int bagOfTokensScore(int[] tokens, int power) {
+        Arrays.sort(tokens);
+        int left = 0;
+        int right = tokens.length-1;
+        int score = 0;
+        int maxScore = 0;
+        while(left<=right){
+            //try face-up use smallest token to gain score
+            if(power>=tokens[left]){
+                power-= tokens[left];
+                score++;
+                left++;
+                maxScore = Math.max(maxScore, score);
+            }else if(score>=1){
+                power += tokens[right];
+                score--;
+                right--;
+            }else{
+                break;
+            }
+        }
+        return maxScore;
+    }
+}
