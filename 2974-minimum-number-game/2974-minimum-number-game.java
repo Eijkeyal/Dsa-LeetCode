31b52f6e85c8180fe8f80cb9f70744e957f4992d
Eijkeyal Pakhrin <209868212+Eijkeyal@users.noreply.github.com>
2026-06-03 23:42:59 +0545

    Time: 5 ms (79.47%), Space: 47 MB (57.26%) - LeetHub

---
+++ 2974-minimum-number-game/2974-minimum-number-game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] numberGame(int[] nums) {
+        Arrays.sort(nums);
+        int[] arr = new int[nums.length];
+        
+        for(int i = 0; i < nums.length; i += 2) {
+            arr[i] = nums[i + 1];     // Bob's number (second smaller)
+            arr[i + 1] = nums[i];     // Alice's number (first smaller)
+        }
+        
+        return arr;
+    }
+}
